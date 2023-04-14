25cc9e6ef86c8ed370ef66daff80275644335c7d
mdefab <defab@live.ca>
2023-04-14 15:56:38 -0400

    Move weight conversion dictionary out of main.py into its own file and add __pycache__ to gitignore

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import streamlit as st
-
-
-data_conversion_dict = {
-    'lbs': {'kg': 0.4536, 'g': 453.592, 'oz': 16, 'Stone': 0.07143},
-    'kg': {'lbs': 2.2046, 'oz': 35.274, 'g': 1000, 'Stone': 0.1575},
-    'g': {'oz': 0.035, 'kg': 0.001, 'lbs': 0.0022, 'Stone': 0.0001575},
-    'oz': {'lbs': 0.0625, 'kg': 0.02835, 'g': 28.3495, 'Stone': 0.0044643},
-    'Stone': {'lbs': 14, 'kg': 6.3503, 'oz': 224, 'g': 6350.29}
-}
+from weight_conversions import data_conversion_dict
 
 
 def conversion_calculator():
