d96c916ea13e96d53bed163f33e653c72fee4915
mdefab <108475969+mdefab@users.noreply.github.com>
2023-04-18 15:49:07 -0400

    Update main.py into module
    Wrap app page specific code in a function that only executes when the main program is executed (i.e. if __name__ =='__main__')

--- main.py
+++ main.py
@@ -2,32 +2,36 @@ import streamlit as st
 from weight_conversions import data_conversion_dict
 
 
-def conversion_calculator():
+def conversion_calculator(convert_from, convert_to, amount):
     try:
-        conversion_value = data_conversion_dict[st.session_state.convert_from][st.session_state.convert_to]
-        return st.session_state.amount * conversion_value
+        conversion_value = data_conversion_dict[convert_from][convert_to]
+        return amount * conversion_value
     except:
         return "Error converting"
 
 
-st.title("Weight Convertor: ")
-if 'convert_from' not in st.session_state:
-    st.session_state.convert_from = "lbs"
+def main():
+    st.set_page_config(page_title="Weight Convertor")
+    st.title("Weight Convertor: ")
+    if 'convert_from' not in st.session_state:
+        st.session_state.convert_from = "lbs"
 
-if 'convert_to' not in st.session_state:
-    st.session_state.convert_to = "kg"
+    if 'convert_to' not in st.session_state:
+        st.session_state.convert_to = "kg"
 
-if 'amount' not in st.session_state:
-    st.session_state.amount = 0
+    if 'amount' not in st.session_state:
+        st.session_state.amount = 0
 
+    st.selectbox("Pick a unit to convert from: ", list(data_conversion_dict), key='convert_from')
+    st.selectbox("Pick a unit to convert to: ", list(data_conversion_dict[st.session_state.convert_from]), key='convert_to')
+    with st.form(key="my_form"): 
+        st.number_input(f"How many {st.session_state.convert_from}?", min_value=0.0, max_value=10000000.0, step=0.01, key='amount')
+        submit = st.form_submit_button(label="submit")
 
-st.selectbox("Pick a unit to convert from: ", list(data_conversion_dict), key='convert_from')
-st.selectbox("Pick a unit to convert to: ", list(data_conversion_dict[st.session_state.convert_from]), key='convert_to')
-with st.form(key="my_form"): 
-    st.number_input(f"How many {st.session_state.convert_from}?", min_value=0.0, max_value=10000000.0, step=0.01, key='amount')
-    submit = st.form_submit_button(label="submit")
+    if submit:
+        result = conversion_calculator(st.session_state.convert_from, st.session_state.convert_to, st.session_state.amount)
+        st.write(f"Result: {st.session_state.amount} {st.session_state.convert_from} is {round(result, 4)}{st.session_state.convert_to}")
 
-if submit:
-    result = conversion_calculator()
-    st.write(f"Result: {st.session_state.amount} {st.session_state.convert_from} is {round(result, 4)}{st.session_state.convert_to}")
+if __name__ == '__main__':
+    main()
 
